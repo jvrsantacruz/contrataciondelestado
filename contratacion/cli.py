6ec0b2d804830b4e9e8fbb62ba70c6cbd4e14681
Javier Santacruz <javier.santacruz.lc@gmail.com>
2013-12-24 21:40:23 +0100

    Adds command line interface

---
+++ contratacion/cli.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""Contratacion
+
+Usage:
+    contratacion fetch [options ...] [--page=<N>] [--store=<FILE>]
+    contratacion parse [options ...] [--store=<FILE>] [--db=<FILE>]
+
+Commands:
+    fetch       Download documents from the web page
+    parse       Parse and store downloaded documents
+
+Options:
+    -h --help       See this help
+    --version       Show version
+    --page N        Skip previous pages when downloading
+    --store FILE    Database file for download store [default: store.sqlite]
+    --db FILE       Database file for processed data [default: db.sqlite]
+    -v --verbose    Increase output verbosity level (Use -vv for debug level)
+"""
+import logging
+
+from docopt import docopt
+
+from . import __version__
+
+
+def fetch_documents(args):
+    from . import fetcher
+
+    page = args.get('--page')
+    store = args.get('--store')
+    fetcher.fetch_documents(store_path=store, page=page)
+
+
+def parse_documents(args):
+    from . import parser
+
+    db = args.get('--db')
+    store = args.get('--store')
+    parser.parse_documents(store_path=store, database_path=db)
+
+
+def print_help():
+    print(__doc__)
+
+
+def print_version():
+    print('contratacion ' + __version__)
+
+
+def configure_logging(args):
+    verbosity = args.get('--verbose')
+    levels = [logging.ERROR, logging.INFO, logging.DEBUG]
+    logging_format = '%(asctime)s %(levelname)-8s %(message)s'
+    logging.basicConfig(level=levels[verbosity], format=logging_format)
+
+
+def main():
+    args = docopt(__doc__, version=__version__)
+
+    configure_logging(args)
+
+    if args['fetch']:
+        fetch_documents(args)
+    elif args['parse']:
+        parse_documents(args)
+    elif args['--help']:
+        print_help()
+    elif args['--version']:
+        print_version()
