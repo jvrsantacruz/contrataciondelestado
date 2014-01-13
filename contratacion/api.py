fb659d49c8aee17afc33d9e25298d7db1ace345f
Javier Santacruz <javier.santacruz.lc@gmail.com>
2014-01-13 23:07:04 +0100

    Joins collection and element REST handlers into the same resources

--- contratacion/api.py
+++ contratacion/api.py
@@ -20,14 +20,13 @@ class Resource(restful.Resource):
 class Licitations(Resource):
     model = models.Licitation
 
-    def get(self):
+    def get(self, id=None):
+        return self._get_all() if id is None else self._get_one(id)
+
+    def _get_all(self):
         return dict(licitations=self.mapper.all())
 
-
-class Licitation(Resource):
-    model = models.Licitation
-
-    def get(self, id):
+    def _get_one(self, id):
         return dict(licitation=self.mapper.get_or_404(id))
 
 
@@ -38,18 +37,13 @@ class Contractors(Resource):
     def mapper(self):
         return mappers.ViewMapper(self.model.contractors(g.db))
 
-    def get(self):
+    def get(self, id=None):
+        return self._get_all() if id is None else self._get_one(id)
+
+    def _get_all(self):
         return dict(contractors=self.mapper.all())
 
-
-class Contractor(Resource):
-    model = models.Party
-
-    @property
-    def mapper(self):
-        return mappers.ViewMapper(self.model.contractors(g.db))
-
-    def get(self, id):
+    def _get_one(self, id):
         return dict(contractor=self.mapper.get_or_404(id))
 
 
@@ -60,40 +54,30 @@ class Contracteds(Resource):
     def mapper(self):
         return mappers.ViewMapper(self.model.contracted(g.db))
 
-    def get(self):
+    def get(self, id=None):
+        return self._get_all() if id is None else self._get_one(id)
+
+    def _get_all(self):
         return dict(contracted=self.mapper.all())
 
-
-class Contracted(Resource):
-    model = models.Party
-
-    @property
-    def mapper(self):
-        return mappers.ViewMapper(self.model.contracted(g.db))
-
-    def get(self, id):
+    def _get_one(self, id):
         return dict(contracted=self.mapper.get_or_404(id))
 
 
 class Parties(Resource):
     model = models.Party
 
-    def get(self):
+    def get(self, id=None):
+        return self._get_all() if id is None else self._get_one(id)
+
+    def _get_all(self):
         return dict(parties=self.mapper.all())
 
-
-class Party(Resource):
-    model = models.Party
-
-    def get(self, id):
+    def _get_one(self, id):
         return dict(party=self.mapper.get_or_404(id))
 
 
-api.add_resource(Licitations, '/licitations')
-api.add_resource(Licitation, '/licitations/<int:id>')
-api.add_resource(Contractors, '/contractors')
-api.add_resource(Contractor, '/contractors/<int:id>')
-api.add_resource(Contracteds, '/contracted')
-api.add_resource(Contracted, '/contracted/<int:id>')
-api.add_resource(Parties, '/parties')
-api.add_resource(Party, '/parties/<int:id>')
+api.add_resource(Licitations, '/licitations', '/licitations/<int:id>')
+api.add_resource(Contractors, '/contractors', '/contractors/<int:id>')
+api.add_resource(Contracteds, '/contracted', '/contracted/<int:id>')
+api.add_resource(Parties, '/parties', '/parties/<int:id>')
